bcae63f692de1e86f18cabbc2548576e4a26891c
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2022-02-26 16:23:24 +1300

    add test for adding bears

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+test('adds a bear', () => {
+  render(<App />)
+  const button = screen.getByText(/add a bear/i)
+  expect(screen.getByText(/[0-9] bears/i)).toHaveTextContent('0 bear')
+  fireEvent.click(button)
+  expect(screen.getByText(/[0-9] bears/i)).toHaveTextContent('1 bear')
 })
